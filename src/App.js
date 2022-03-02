a5c7c750e7f279f88cbe408f419349efd7f823db
noui61 <noui@metavault.org>
2022-03-02 04:41:37 +0300

    herosection-infosection add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import  { BrowserRouter as Router } from 'react-router-dom'
+import Home from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <h1>What's uppppp</h1>
-    </div>
+    <Router>
+      <Home/>
+    </Router>
   );
 }
 
